a17c9bdf964b33252a7b03dee1ab29bad1d8f994
Maciej Sypien <maciejsypien@gmail.com>
2013-12-25 17:45:56 +0100

    Improve views; Add results template

--- views.py
+++ views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-
-from polls.models import Poll
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 	context = {'latest_poll_list': latest_poll_list}
 	return render(request, 'polls/index.html', context)
 
-def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s.", poll_id)
-
 def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s", poll_id)
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s", poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the poll voting form.
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't selected a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Black button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 def detail(request, poll_id):
 	poll = get_object_or_404(Poll, pk=poll_id)
 	return render(request, 'polls/detail.html', {'poll': poll})
+
